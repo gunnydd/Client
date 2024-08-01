8f2ff73ffca4bff55f9ac8d7f3cc9a055740f995
Kunhee <kunheehwang97@gmail.com>
2024-08-01 13:23:33 -0400

    This is finalrrrryyyrrrryrrrrrr

--- rasp.py
+++ rasp.py
@@ -206,18 +206,5 @@ def shutdown():
 
 
 if __name__ == "__main__":
-  ## LEt's test it out ! 
-  ## Let's test morder!
-  ## HELo check this out ! 
-  ## Hello !
-  ## dddd...
-  ##????
-  ## let's hope it works
-  # plz ! 
-  #  plz! ! 
-  # plz ! 
-  # FKUNHEE HWANG
-  # This is actually fun ! 
-  # Fuck you Thunder ! 
-  # you can !! 
+  ### 제발좀 되라 씨...앙....
   app.run(debug=True, host='0.0.0.0', port=5001)
